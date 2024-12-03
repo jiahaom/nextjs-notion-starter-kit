f6740e6a0b9d75f6a3ee73e57a40b9b9a4a21c71
Jiahao <32929553+jiahaom@users.noreply.github.com>
2024-12-03 12:49:15 +0000

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -6,7 +6,7 @@ export default siteConfig({
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: '73bf1ac1df0d4f2984fab7c10664ff60',
 
   // basic site info (required)
   name: 'Trip Pizza',
